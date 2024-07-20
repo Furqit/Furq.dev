16bc044c73578d087353da2dc8b9bbc76d9136d7
Furq <furqanahmedk27@gmail.com>
2024-07-20 07:24:02 +0500

    website revamped, simplified

--- js/app.js
+++ js/app.js
@@ -1,50 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+document.getElementById("cards").onmousemove = (e) => {
+  for (const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
 
-  const numBalls = 50;
-  const balls = [];
-
-  for (let i = 0; i < numBalls; i++) {
-    let ball = document.createElement("div");
-    ball.classList.add("ball");
-    ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-    ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-    ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-    ball.style.transform = `scale(${Math.random()})`;
-    ball.style.width = `${Math.random()}em`;
-    ball.style.height = ball.style.width;
-
-    balls.push(ball);
-    document.body.append(ball);
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
   }
-
-  balls.forEach((el, i) => {
-    let to = {
-      x: Math.random() * (i % 2 === 0 ? -11 : 11),
-      y: Math.random() * 12,
-    };
-
-    let anim = el.animate(
-      [
-        { transform: "translate(0, 0)" },
-        { transform: `translate(${to.x}rem, ${to.y}rem)` },
-      ],
-      {
-        duration: (Math.random() + 1) * 2000,
-        direction: "alternate",
-        fill: "both",
-        iterations: Infinity,
-        easing: "ease-in-out",
-      }
-    );
-  });
-});
-document.addEventListener("keydown", function (event) {
-  if (event.ctrlKey) {
-    event.preventDefault();
-  }
-
-  if (event.keyCode == 123) {
-    event.preventDefault();
-  }
-});
+};
